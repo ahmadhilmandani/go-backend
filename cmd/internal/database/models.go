d235b10392e67731bcbe101fea516dced9ad2094
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2025-08-06 05:53:54 +0700

    add:  models event, attendees, users

---
+++ cmd/internal/database/models.go
@@ -0,0 +1,17 @@
+package database
+
+import "database/sql"
+
+type Models struct {
+	User      UserModel
+	Event     EventModel
+	Attendees AttendeesModel
+}
+
+func NewModles(db *sql.DB) Models {
+	return Models{
+		User:      UserModel{DB: db},
+		Event:     EventModel{DB: db},
+		Attendees: AttendeesModel{DB: db},
+	}
+}
